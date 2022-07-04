262fd787ddf285833defd0514400887f33a1c481
BabichOlesya <93949245+BabichOlesya@users.noreply.github.com>
2022-07-05 00:25:58 +0200

    Add Buttons

--- FamilyBudget/Expenditure/ExpenditureViewController.swift
+++ FamilyBudget/Expenditure/ExpenditureViewController.swift
@@ -12,7 +12,39 @@ class ExpenditureViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        addConstraints()
     }
     
+    private lazy var addCostButton: UIButton = {
+        let addCostButton = UIButton()
+        addCostButton.setTitle("Добавить", for: .normal)
+        addCostButton.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
+        addCostButton.backgroundColor = .systemGray2
+        addCostButton.layer.shadowColor = UIColor.black.cgColor
+        addCostButton.layer.shadowOffset = CGSize(width: 2, height: 2)
+        addCostButton.layer.cornerRadius = 7
+        addCostButton.layer.shadowOpacity = 1
+        addCostButton.translatesAutoresizingMaskIntoConstraints = false
+        return addCostButton
+    }()
+    
+    @objc func buttonPressed() {
+    }
+    
+    private func addConstraints() {
+        
+        view.addSubview(addCostButton)
+
+        var constraints = [NSLayoutConstraint]()
+        
+        constraints.append(addCostButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50))
+        constraints.append(addCostButton.centerXAnchor.constraint(equalTo: view.centerXAnchor))
+        constraints.append(addCostButton.heightAnchor.constraint(equalToConstant: 50))
+        constraints.append(addCostButton.widthAnchor.constraint(equalToConstant: 180))
+
+
+
+        NSLayoutConstraint.activate(constraints)
+    }
 
 }
--- FamilyBudget/Income/IncomeViewController.swift
+++ FamilyBudget/Income/IncomeViewController.swift
@@ -12,6 +12,38 @@ class IncomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        addConstraints()
     }
 
+    private lazy var addIncomeButton: UIButton = {
+        let addIncomeButton = UIButton()
+        addIncomeButton.setTitle("Добавить", for: .normal)
+        addIncomeButton.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
+        addIncomeButton.backgroundColor = .systemGray2
+        addIncomeButton.layer.shadowColor = UIColor.black.cgColor
+        addIncomeButton.layer.shadowOffset = CGSize(width: 2, height: 2)
+        addIncomeButton.layer.cornerRadius = 7
+        addIncomeButton.layer.shadowOpacity = 1
+        addIncomeButton.translatesAutoresizingMaskIntoConstraints = false
+        return addIncomeButton
+    }()
+    
+    @objc func buttonPressed() {
+    }
+    
+    private func addConstraints() {
+        
+        view.addSubview(addIncomeButton)
+
+        var constraints = [NSLayoutConstraint]()
+        
+        constraints.append(addIncomeButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50))
+        constraints.append(addIncomeButton.centerXAnchor.constraint(equalTo: view.centerXAnchor))
+        constraints.append(addIncomeButton.heightAnchor.constraint(equalToConstant: 50))
+        constraints.append(addIncomeButton.widthAnchor.constraint(equalToConstant: 180))
+
+
+
+        NSLayoutConstraint.activate(constraints)
+    }
 }
